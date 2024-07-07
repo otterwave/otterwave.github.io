706762925757469c09f47a91581da1d82c1501f0
softmaxer <sriram.vadlamani@proton.me>
2024-07-07 21:41:25 +0200

    Add main title component with logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import ProfileButton from "@/components/buttons/profile-button";
+import { MainTitle } from "@/components/title";
 export default function Home() {
   return (
     <>
       <div className="flex flex-col justify-between items-center gap-8">
-        <h1 className="text-7xl">Otterwave</h1>
+        <MainTitle />
         <h3>Your guide to marketing automation</h3>
       </div>
       <div className="flex flex-row justify-between items-center gap-24">
         <ProfileButton action="discover" path="#" />
-        <ProfileButton action="get in touch" path="#" />
+        <ProfileButton
+          action="get in touch"
+          path="mailto:sales.otterwave@gmail.com"
+        />
       </div>
     </>
   );
