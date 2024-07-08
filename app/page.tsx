dfe04d3aad11b1d3b8fa59a9ffcfb24f6db06017
Hugo <hugo.prossaird@raccourci.fr>
2024-07-08 07:40:36 +0200

    add desc + maj

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,12 @@ export default function Home() {
       <div className="flex flex-col justify-between items-center gap-8">
         <MainTitle />
         <h3>Your guide to marketing automation</h3>
+        <p>Boost your sales, save time and money ! </p>
       </div>
       <div className="flex flex-row justify-between items-center gap-24">
-        <ProfileButton action="discover" path="#" />
+        <ProfileButton action="Discover" path="#" />
         <ProfileButton
-          action="get in touch"
+          action="Get in touch"
           path="mailto:sales.otterwave@gmail.com"
         />
       </div>
