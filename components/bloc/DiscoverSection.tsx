9c3ff0a97c5bd1d040a2fe44867e86faabb33b81
Hugo <hugo.prossaird@raccourci.fr>
2024-07-14 08:20:17 +0200

    fix - &apos;

--- components/bloc/DiscoverSection.tsx
+++ components/bloc/DiscoverSection.tsx
@@ -10,7 +10,7 @@ const DiscoverSection: React.FC<DiscoverSectionProps> = ({ onBack }) => {
     <div className="flex flex-col items-center justify-center p-8 text-white m-auto">
       <h1>Découvrez nos solutions</h1>
       <p className="max-w-3xl m-auto mb-2 mt-8 text-center py-8">
-        Explorez notre gamme de solutions de marketing automation conçues pour améliorer l'efficacité de vos campagnes, augmenter vos leads, et simplifier vos processus marketing.
+        Explorez notre gamme de solutions de marketing automation conçues pour améliorer l&apos;efficacité de vos campagnes, augmenter vos leads, et simplifier vos processus marketing.
       </p>
       <OWButton action="Retour à l'accueil" color="secondary" onClick={onBack} />
     </div>
