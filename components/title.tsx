702f3e8005cd5ee5b4ff41cbea400cc3ad929356
Hugo <hugo.prossaird@raccourci.fr>
2024-07-28 16:15:31 +0200

    fix - logo

--- components/title.tsx
+++ components/title.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image'
+
 export function MainTitle() {
   return (
     <div className="flex flex-row items-center mt-3">
-      <img className="h-16" src="logo_otterwave/png/logo_otterwave_blanc_titre_slogan.png" alt="logo otterwave"></img>
+      <Image width="240" height="240" src="/logo_otterwave/png/logo_otterwave_blanc_titre_slogan.png" alt="logo otterwave"/>
     </div>
   );
 }
